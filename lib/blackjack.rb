2c40742f7ac715f3b052f10897f4dad386730820
chugachski <natesmith.copper@gmail.com>
2016-04-18 14:53:20 -0800

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,53 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  return card_total # otherwise would return a string from #display_card_total
 end
 
-def hit?
-  # code hit? here
+# argument: have to know what current card total is before we can update after
+# hitting or leave the same if we stay
+# test will say 'wrong number of arguments (1 for 0)' b/c rspec passed an arg
+# that wasn't in our method
+def hit?(card_total)
+  prompt_user
+  player_choice = get_user_input
+  if player_choice == "h"
+    card_total = deal_card + card_total # or 'card_total += deal_card'
+  elsif player_choice == "s"
+    card_total # do not update 'card_total'
+  else
+    invalid_command(card_total) # other input will call #hit? again
+  end
 end
 
-def invalid_command
-  # code invalid_command here
+def invalid_command(card_total)
+  puts "Hey Dicknose, enter 'h' to hit or 's' to stay!"
+  puts "Your cards add up to #{card_total}"
+  hit?(card_total)
 end
 
 #####################################################
@@ -39,6 +55,16 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round # set this to card_total otherwise will get
+  #undefined local variable error
+
+  until card_total > 21 do
+    card_total = hit?(card_total) # update card_total
+    display_card_total(card_total)
+  end
+  
+  end_game(card_total)
 end
-    
+
+runner # call runner to play
